703eaafa003fbc2e741c668a5e8f6df1b513e7e8
Kamari <kmoore971@gmail.com>
2023-11-09 11:20:05 -0600

    Add Storage module

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -2,7 +2,34 @@ import Project from './Project'
 import Task from './Task'
 import TodoList from './TodoList'
 
-// Used to store the data and create the buckets and thier content
+// Used to store the data and create the buckets and thier content AKA the entire ToDoList Data
 export default class Storage {
+    // save the app to local storage 
+    static saveTodoList(data) {
+        localStorage.setItem('todoList', JSON.stringify(data))
+    }
 
+    // intialize a new ToDoList instance with all the data 
+    static getTodoList() {
+        const todoList = Object.assign(
+            new TodoList(),
+            JSON.parse(localStorage.getItem('todoList'))
+        )
+
+        todoList.setProjects(
+            todoList
+                .getProjects()
+                .map((project) => Object.assign(new Project(), project))
+        )
+
+        todoList
+            .getProjects()
+            .forEach((project) =>
+                project.setTasks(
+                    project.getTasks().map((task) => Object.assign(new Task(), task))
+                )
+            )
+
+        return todoList
+    }
 }
